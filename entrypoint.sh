7a3d0870d28d1a51d421e240575bf0928cb524ee
jduncan <jduncan@redhat.com>
2019-04-15 22:15:20 -0400

    working on container entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 # Helper script to automate the build and deployment of Sphinx content for a specific workshop
 
 make dirhtml
+cp -r _build/dirhtml/_static .
 cd _build/dirhtml && python -m SimpleHTTPServer 8080
