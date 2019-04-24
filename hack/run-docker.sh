bdee858e0e439580aaec779db85f6e19f87c8f3f
Chris Reynolds <creynold@redhat.com>
2019-04-23 20:36:23 -0400

    adding node ip addresses for 3 hosts

--- hack/run-docker.sh
+++ hack/run-docker.sh
@@ -6,13 +6,24 @@ WORKSHOP_NAME=$1
 QUAY_USER=jduncan
 TMP_FILE=/tmp/lab_guide_id_$WORKSHOP_NAME
 ETH_INT=ens33
+PRIVATE_IP=$(ip addr show $ETH_INT | grep 'inet ' | awk '{ print $2 }' | awk -F/ '{ print $1 }')
+STUDENT_NAME=student1
 NODE_1_IP=$(cat /home/student1/lightbulb/inventory.ini | grep node-1 | awk '{ print $2 }' | awk -F'=' '{ print $2 }')
 NODE_2_IP=$(cat /home/student1/lightbulb/inventory.ini | grep node-2 | awk '{ print $2 }' | awk -F'=' '{ print $2 }')
 NODE_3_IP=$(cat /home/student1/lightbulb/inventory.ini | grep node-3 | awk '{ print $2 }' | awk -F'=' '{ print $2 }')
 
-echo $NODE_1_IP
-echo $NODE_2_IP
-echo $NODE_3_IP
+# Create the docker run env.list file
+echo "creating the env.list file"
+
+echo $WORKSHOP_NAME > /tmp/env.list
+echo $QUAY_USER >> /tmp/env.list
+echo $TMP_FILE >> /tmp/env.list
+echo $ETH_INT >> /tmp/env.list
+echo $PRIVATE_IP >> /tmp/env.list
+echo $STUDENT_NAME >> /tmp/env.list
+echo $NODE_1_IP >> /tmp/env.list
+echo $NODE_2_IP >> /tmp/env.list
+echo $NODE_3_IP >> /tmp/env.list
 
 stop_local() {
   if [ -f $TMP_FILE ]; then
@@ -30,9 +41,7 @@ stop_local() {
 
 start_local() {
   echo "Preparing new lab guide for $WORKSHOP_NAME"
-  PRIVATE_IP=$(ip addr show $ETH_INT | grep 'inet ' | awk '{ print $2 }' | awk -F/ '{ print $1 }')
-  STUDENT_NAME=student1
-  docker run -d -e WORKSHOP_NAME=$WORKSHOP_NAME -e PRIVATE_IP=$PRIVATE_IP -e STUDENT_NAME=$STUDENT_NAME -p 8080:8080 quay.io/$QUAY_USER/operator-workshop-lab-guide-$WORKSHOP_NAME &> $TMP_FILE
+  docker run -d --env-file /tmp/env.list -p 8080:8080 quay.io/$QUAY_USER/operator-workshop-lab-guide-$WORKSHOP_NAME &> $TMP_FILE
   if [ $? -eq 0 ]; then
     LAB_CONTAINER=$(cat $TMP_FILE | cut -c1-12)
     echo "$WORKSHOP_NAME is running as container ID $LAB_CONTAINER and is avaiable at http://localhost:8080"
