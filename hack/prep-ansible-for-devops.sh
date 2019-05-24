f865b8fd110226e78f352ce30b103299522003bb
jduncan <jduncan@redhat.com>
2019-05-24 13:20:13 -0400

    making run script more universal

---
+++ hack/prep-ansible-for-devops.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+ENV_FILE=$1
+
+echo "Preparing environment variables for $WORKSHOP_NAME lab guide"
+
+STUDENT_NAME=$(grep student /etc/passwd | awk -F':' '{ print $1 }')
+if [[ ${#STUDENT_NAME} -eq 0 ]];then
+  STUDENT_NAME=student1
+fi
+INVENTORY_FILE=/home/$STUDENT_NAME/devops-workshop/lab_inventory/hosts
+if [ -f $INVENTORY_FILE ];then
+  CONTROL_PRIVATE_IP=$(grep 'ansible ansible_host' $INVENTORY_FILE | awk -F'=' '{ print $2 }')
+  STUDENT_PASS=$(grep ansible_ssh_pass $INVENTORY_FILE | awk -F= '{ print $2 }')
+  CONTROL_PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+  NODE_1_IP=$(grep node1 $INVENTORY_FILE | awk -F'=' '{ print $2 }')
+  NODE_2_IP=$(grep node2 $INVENTORY_FILE | awk -F'=' '{ print $2 }')
+  NODE_3_IP=$(grep node3 $INVENTORY_FILE | awk -F'=' '{ print $2 }')
+  NODE_4_IP=$(grep node4 $INVENTORY_FILE | awk -F'=' '{ print $2 }')
+else
+  CONTROL_PRIVATE_IP=10.0.0.1
+  STUDENT_PASS=redhat01
+  CONTROL_PUBLIC_IP=192.168.0.1
+  NODE_1_IP=10.0.0.2
+  NODE_2_IP=10.0.0.3
+  NODE_3_IP=10.0.0.4
+  NODE_4_IP=10.0.0.5
+fi
+
+echo "CONTROL_PRIVATE_IP="$CONTROL_PRIVATE_IP >> $ENV_FILE
+echo "CONTROL_PUBLIC_IP="$CONTROL_PUBLIC_IP >> $ENV_FILE
+echo "STUDENT_NAME="$STUDENT_NAME >> $ENV_FILE
+echo "STUDENT_PASS="$STUDENT_PASS >> $ENV_FILE
+echo "NODE_1_IP="$NODE_1_IP >> $ENV_FILE
+echo "NODE_2_IP="$NODE_2_IP >> $ENV_FILE
+echo "NODE_3_IP="$NODE_3_IP >> $ENV_FILE
+echo "NODE_4_IP="$NODE_4_IP >> $ENV_FILE
--- hack/run.sh
+++ hack/run.sh
@@ -3,9 +3,21 @@
 # It assumes docker at this point.
 
 WORKSHOP_NAME=$1
+RUN_TYPE=$2
 QUAY_USER=jduncan
 TMP_FILE=/tmp/lab_guide_id_$WORKSHOP_NAME
-ETH_INT=ens33
+ENV_FILE=/tmp/env.list
+CONTAINER_IMAGE=quay.io/$QUAY_USER/operator-workshop-lab-guide-$WORKSHOP_NAME
+if [[ ${#3} -eq 0 ]];then
+  PORT=8080
+else
+  PORT=$3
+fi
+
+# Create the docker run env.list file
+echo "creating the environment variables file at $ENV_FILE"
+
+echo "WORKSHOP_NAME="$WORKSHOP_NAME > $ENV_FILE
 
 stop_local() {
   if [ -f $TMP_FILE ]; then
@@ -23,16 +35,26 @@ stop_local() {
 
 start_local() {
   echo "Preparing new lab guide for $WORKSHOP_NAME"
-  PRIVATE_IP=$(ip addr show $ETH_INT | grep 'inet ' | awk '{ print $2 }' | awk -F/ '{ print $1 }')
-  STUDENT_NAME=student1
-  docker run -d -e WORKSHOP_NAME=$WORKSHOP_NAME -e PRIVATE_IP=$PRIVATE_IP -e STUDENT_NAME=$STUDENT_NAME -p 8080:8080 quay.io/$QUAY_USER/operator-workshop-lab-guide-$WORKSHOP_NAME &> $TMP_FILE
+  if [[ $RUN_TYPE = 'start' ]]; then
+    docker pull $CONTAINER_IMAGE
+  fi
+  ENV_PREP_SCRIPT=prep-$WORKSHOP_NAME.sh
+  if [ -f ./hack/$ENV_PREP_SCRIPT ]; then
+    ./hack/$ENV_PREP_SCRIPT $ENV_FILE
+  fi
+  docker run -d --env-file /tmp/env.list --name lab_guide_$WORKSHOP_NAME -p $PORT:8080 $CONTAINER_IMAGE &> $TMP_FILE
   if [ $? -eq 0 ]; then
     LAB_CONTAINER=$(cat $TMP_FILE | cut -c1-12)
-    echo "$WORKSHOP_NAME is running as container ID $LAB_CONTAINER and is avaiable at http://localhost:8080"
+    echo "$WORKSHOP_NAME is running as container ID $LAB_CONTAINER and is avaiable at http://localhost:$PORT"
   fi
+  rm -f $ENV_FILE
 }
 
 case $2 in
+  start)
+    stop_local
+    start_local
+  ;;
   local)
     stop_local
     start_local
@@ -41,7 +63,7 @@ case $2 in
     stop_local
   ;;
   *)
-    echo "Valid locations: 'local'"
     echo "Usage: ./run.sh <WORKSHOP_NAME> <TARGET>"
+    echo "Valid targets: 'local', 'start', 'stop'"
   ;;
 esac
