96fd83db99097b2771911f927323a33bdf3a12f6
James Harmison <jharmison@redhat.com>
2020-06-15 10:15:29 -0400

    Rewrote build.sh to be more succinct
    Now more succinct, uses podman instead of docker, and can push to repo from local builds

--- hack/build.sh
+++ hack/build.sh
@@ -1,30 +1,30 @@
 #! /usr/bin/env bash
 # Helper script to build container images for each workshop
-# if using podman, be sure to install the podman-docker package
-# also, if using podman, up th
 
 WORKSHOP_NAME=$1
-if [[ ${#3} -eq 0 ]];then
-  QUAY_PROJECT=creynold
-else
-  QUAY_PROJECT=$3
+LOCATION=${2:local}
+QUAY_PROJECT=${3:-rhcreynold}
+
+cd $(dirname $(realpath $0))/..
+if [ -f .quay_creds ]; then
+  LOCATION=quay
+  . .quay_creds
 fi
 
-
-case $2 in
+case $LOCATION in
   local)
-    docker build --build-arg workshop_name=$WORKSHOP_NAME \
-    -t quay.io/$QUAY_PROJECT/operator-workshop-lab-guide-$WORKSHOP_NAME \
-    .
+    podman build --build-arg workshop_name=$WORKSHOP_NAME \
+      -t quay.io/$QUAY_PROJECT/operator-workshop-lab-guide-$WORKSHOP_NAME .
   ;;
   quay)
     # designed to be used by travis-ci, where the docker_* variables are defined
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin quay.io
-    docker build --build-arg workshop_name=$WORKSHOP_NAME \
-    -t quay.io/$QUAY_PROJECT/operator-workshop-lab-guide-$WORKSHOP_NAME .
-    docker push quay.io/$QUAY_PROJECT/operator-workshop-lab-guide-$WORKSHOP_NAME
+    echo "$DOCKER_PASSWORD" | podman login -u "$DOCKER_USERNAME" --password-stdin quay.io
+
+    podman build --build-arg workshop_name=$WORKSHOP_NAME \
+      -t quay.io/$QUAY_PROJECT/operator-workshop-lab-guide-$WORKSHOP_NAME .
+    podman push quay.io/$QUAY_PROJECT/operator-workshop-lab-guide-$WORKSHOP_NAME
   ;;
   *)
-    echo "usage: ./hack/build.sh <WORKSHOP_NAME> <LOCATION>"
+    echo "usage: ./hack/build.sh WORKSHOP_NAME [local|quay] [QUAY_PROJECT]"
   ;;
 esac
